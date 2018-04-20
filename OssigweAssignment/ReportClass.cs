ddd7e70467ee8c3545fc73a417985eac02346d33
chrisokhamera <chrisokhamera@gmail.com>
2018-04-20 12:16:20 +0100

    Correction

---
+++ OssigweAssignment/ReportClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEGroup5
+{
+   public  class ReportClass
+    {
+
+        public void initializeReport()
+        {
+
+        }
+    }
+
+}
